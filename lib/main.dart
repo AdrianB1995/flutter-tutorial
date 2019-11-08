721b607cc8772861b4826b55bc94c710b3acf8c2
AdrianB1995 <adrianfrancisco.borrego@gmail.com>
2019-11-08 14:28:10 -0500

    Using expanded widgets within Rows.

--- lib/main.dart
+++ lib/main.dart
@@ -18,96 +18,26 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.blue[600]
       ),
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                child: Text('1'),
-                color: Colors.red[100],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('2'),
-                color: Colors.red[200],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('3'),
-                color: Colors.red[300],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('4'),
-                color: Colors.red[400],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('5'),
-                color: Colors.red[500],
-                padding: EdgeInsets.all(10.0),
-              )
-            ],
+          Expanded(
+            flex: 3,
+              child: Image.asset('assets/optimus_prime.jpg')
           ),
-          Column(
-            children: <Widget>[
-              Container(
-                child: Text('1'),
-                color: Colors.blue[100],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('2'),
-                color: Colors.blue[200],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('3'),
-                color: Colors.blue[300],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('4'),
-                color: Colors.blue[400],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('5'),
-                color: Colors.blue[500],
-                padding: EdgeInsets.all(10.0),
-              )
-            ],
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: Text('2'),
+              color: Colors.blue,
+              padding: EdgeInsets.all(10.0),
+            ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                child: Text('1'),
-                color: Colors.orange[100],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('2'),
-                color: Colors.orange[200],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('3'),
-                color: Colors.orange[300],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('4'),
-                color: Colors.orange[400],
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                child: Text('5'),
-                color: Colors.orange[500],
-                padding: EdgeInsets.all(10.0),
-              )
-            ],
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: Text('1'),
+              color: Colors.red,
+              padding: EdgeInsets.all(10.0),
+            ),
           )
         ],
       ),
