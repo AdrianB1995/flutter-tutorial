7411371136a13fb3dd9cf4c64ed525b462a77c01
Adrian <adrianfrancisco.borrego@gmail.com>
2019-11-11 22:59:40 -0500

    - Using inline function to create Card Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,35 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'Adrian Borrego', text: 'Never stop pushing for a better future!'),
     Quote(author: 'Adrian Borrego', text: 'Never forget where you came from!'),
   ];
+  
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +57,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: list.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: list.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
