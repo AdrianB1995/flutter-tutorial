4c639fc4266602b05114542b71effd1167062ab3
Adrian <adrianfrancisco.borrego@gmail.com>
2019-11-12 22:19:53 -0500

    - Demo of async code

--- lib/pages/Location.dart
+++ lib/pages/Location.dart
@@ -6,8 +6,33 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+
+    //Simulate network request for a username to demonstrate dependent code!
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'Adrian';
+    });
+
+    //Simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'The best person in the world!';
+    });
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    print('This line should print no matter what even is getData() is not done!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build state function running');
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -15,9 +40,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose location'),
         centerTitle: true,
         elevation: 0.0,
-      ),
-      body: Text(
-        'location screen'
       ),
     );
   }
